2e86deb09757da11f87926dbf4894882efdfa035
Frans van Buul <frans.vanbuul@axoniq.io>
2018-07-24 09:04:12 +0200

    config improvements

--- src/main/java/io/axoniq/demo/sqwebinar/SqWebinarApplication.java
+++ src/main/java/io/axoniq/demo/sqwebinar/SqWebinarApplication.java
@@ -1,5 +1,10 @@
 package io.axoniq.demo.sqwebinar;
 
+import com.thoughtworks.xstream.XStream;
+import org.axonframework.config.EventProcessingConfiguration;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.serialization.xml.XStreamSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,4 +14,19 @@ public class SqWebinarApplication {
     public static void main(String[] args) {
         SpringApplication.run(SqWebinarApplication.class, args);
     }
+
+    @Autowired
+    public void configure(EventProcessingConfiguration configuration) {
+        configuration.usingTrackingProcessors();
+    }
+
+    @Autowired
+    public void configure(Serializer serializer) {
+        if(serializer instanceof XStreamSerializer) {
+            XStream xStream = ((XStreamSerializer)serializer).getXStream();
+            XStream.setupDefaultSecurity(xStream);
+            xStream.allowTypesByWildcard(new String[] { "io.axoniq.demo.**", "org.axonframework.**" });
+        }
+    }
+
 }
